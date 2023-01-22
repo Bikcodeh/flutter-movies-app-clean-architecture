9ee4c397f5ce02f00b36e7b6164314fbf8fb13cb
Bikcode <bikcodeh@gmail.com>
2023-01-22 10:50:13 -0500

    implements function to read session id from storage

--- lib/app/data/repository/authentication_repository_impl.dart
+++ lib/app/data/repository/authentication_repository_impl.dart
@@ -1,10 +1,21 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import '../../domain/models/user.dart';
 import '../../domain/repository/authentication_repository.dart';
 
+const _key = 'session_id';
+
 class AuthenticationRepositoryImpl implements AuthenticationRepository {
+  final FlutterSecureStorage _flutterSecureStorage;
+
+  AuthenticationRepositoryImpl(this._flutterSecureStorage);
+
   @override
   Future<User?> getUserData() => Future.value(null);
 
   @override
-  Future<bool> get isSignedIn => Future.value(true);
+  Future<bool> get isSignedIn async {
+    final sessionId = await _flutterSecureStorage.read(key: _key);
+    return sessionId != null;
+  }
 }
