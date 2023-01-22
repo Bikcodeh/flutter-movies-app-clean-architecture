e2580cb6d3c4cf9735e05cfb80c8278940367e31
Bikcode <bikcodeh@gmail.com>
2023-01-22 07:23:37 -0500

    implements to handle user authenticated and wheter user has data

--- lib/app/presentation/routes/movies_routes.dart
+++ lib/app/presentation/routes/movies_routes.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/widgets.dart';
 
+import '../modules/home/views/home_view.dart';
+import '../modules/signin/views/sign_in_view.dart';
 import '../modules/splash/views/splash_view.dart';
 import 'routes.dart';
 
 Map<String, Widget Function(BuildContext)> get appRoutes {
-  return {Routes.splash: (context) => const SplashView()};
+  return {
+    Routes.splash: (context) => const SplashView(),
+    Routes.signIn: (context) => const SignInView(),
+    Routes.home: (context) => const HomeView(),
+  };
 }
