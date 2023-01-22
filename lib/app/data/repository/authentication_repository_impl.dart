2cf40927734180c719435b2b6d24d7810e88dcbe
Bikcode <bikcodeh@gmail.com>
2023-01-22 18:00:58 -0500

    partial implmenetation to submit the form when the user does sign in

--- lib/app/data/repository/authentication_repository_impl.dart
+++ lib/app/data/repository/authentication_repository_impl.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+import '../../domain/common/either.dart';
+import '../../domain/common/error.dart';
 import '../../domain/models/user.dart';
 import '../../domain/repository/authentication_repository.dart';
 
@@ -17,5 +19,19 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository {
   Future<bool> get isSignedIn async {
     final sessionId = await _flutterSecureStorage.read(key: _key);
     return sessionId != null;
+  }
+
+  @override
+  Future<Either<Failure, User>> signIn(
+    String username,
+    String password,
+  ) async {
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
+    if (username != 'test' && password != '12345') {
+      return Either.left(Failure.notFound);
+    }
+    return Either.right(User());
   }
 }
