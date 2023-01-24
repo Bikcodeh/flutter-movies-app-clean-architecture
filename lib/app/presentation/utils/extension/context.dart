b6f56b2c9a2f467bccac9f9c9325d3b58bc52769
Bikcode <bikcodeh@gmail.com>
2023-01-24 10:35:49 -0500

    add provider lib and replace injector for provider

--- lib/app/presentation/utils/extension/context.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import '../../../../main.dart';
-
-extension ContextExtension on BuildContext {
-  Injector get injector {
-    return Injector.of(this);
-  }
-}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart';
+import 'package:provider/provider.dart';
 
 import 'app/data/repository/authentication_repository_impl.dart';
 import 'app/data/repository/connectivity_repository_impl.dart';
@@ -13,43 +14,25 @@ import 'app/domain/repository/connectivity_repository.dart';
 import 'app/movies_app.dart';
 
 void main() {
-  runApp(Injector(
-    connectivityRepository: ConnectivityRepositoryImpl(
-      Connectivity(),
-      InternetChecker(),
-    ),
-    authenticationRepository: AuthenticationRepositoryImpl(
-      const FlutterSecureStorage(),
-      AuthenticationApi(
-        Http(
-          baseUrl: 'https://api.themoviedb.org/3',
-          apiKey: '4662e7a7fe13c9d91c80552e10a09dc1',
-          client: Client(),
+  runApp(
+    Provider<ConnectivityRepository>(
+      create: ((context) => ConnectivityRepositoryImpl(
+            Connectivity(),
+            InternetChecker(),
+          )),
+      child: Provider<AuthenticationRepository>(
+        create: (context) => AuthenticationRepositoryImpl(
+          const FlutterSecureStorage(),
+          AuthenticationApi(
+            Http(
+              baseUrl: 'https://api.themoviedb.org/3',
+              apiKey: '4662e7a7fe13c9d91c80552e10a09dc1',
+              client: Client(),
+            ),
+          ),
         ),
+        child: const MoviesApp(),
       ),
     ),
-    child: const MoviesApp(),
-  ));
-}
-
-class Injector extends InheritedWidget {
-  const Injector({
-    super.key,
-    required super.child,
-    required this.authenticationRepository,
-    required this.connectivityRepository,
-  });
-
-  final ConnectivityRepository connectivityRepository;
-  final AuthenticationRepository authenticationRepository;
-
-  @override
-  // ignore: avoid_renaming_method_parameters
-  bool updateShouldNotify(_) => false;
-
-  static Injector of(BuildContext context) {
-    final injector = context.dependOnInheritedWidgetOfExactType<Injector>();
-    assert(injector != null, 'Injector could not be found');
-    return injector!;
-  }
+  );
 }
